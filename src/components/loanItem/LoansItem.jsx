0a5798b7858ea9a2abe6675c41881799f20623c0
Yurii04UA <uyriparaka@gmail.com>
2022-06-13 22:44:44 +0300

    added form and some info

--- src/components/loanItem/LoansItem.jsx
+++ src/components/loanItem/LoansItem.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoansItem.scss";
 
-const loansItem = ({data}) => {
-  const {id,title,} = data
-  console.log(id);
+const LoansItem = ({ data }) => {
+  const { title, id, term_remaining } = data;
+  const [amount, setAmount] = useState(data.amount);
+  const [available, setAviable] = useState(data.available);
+  const [term, setTerm] = useState((data.term_remaining / 86400).toFixed(1));
   return (
-    <li>
-      <div>
-        <p>Loan name {id} </p>
-        <p>Loan detalis</p>
+    <>
+      <li>
+        <div>
+          <h2>Loan name {id} </h2>
+          <p>Available credit limit : ${amount}</p>
+          <p>available amount : ${available}</p>
+        </div>
+        <div className="buttons">
+          <span>invest</span>
+          <button className="btn btn-warning">INVEST</button>
+        </div>
+      </li>
+      <div className="modalWindow w-75">
+        <h2>Invest in Loan {id}</h2>
+        <p>{title}</p>
+        <p>Amount available ${available}</p>
+        <p>Loan ends in : {term} days</p>
+        <form>
+          <h4>Investment amount</h4>
+          <input className="input-group-sm" type="text" />
+          <button className="btn btn-warning">ivest</button>
+        </form>
       </div>
-      <div className="buttons">
-        <span>invest</span>
-        <button className="btn btn-warning">INVEST</button>
-      </div>
-    </li>
+    </>
   );
 };
 
-export default loansItem;
+export default LoansItem;
--- src/components/loans/Loans.jsx
+++ src/components/loans/Loans.jsx
@@ -2,48 +2,20 @@ import React, { useState } from "react";
 import LoansItem from "../loanItem/LoansItem";
 import "./Loans.scss";
 
+const Loans = ({ data }) => {
+  const [total, useTotal] = useState(238456);
+ console.log(data);
 
-const Loans = ({data}) => {
- const [total,useTotal] = useState(238456)
 
-  console.log(data);
+  const loanList = data.map((item, i) => {
+    return <LoansItem data={data[i]} key={i} />;
+  });
+
   return (
-      <div className="wrapper">
-        <ul>
-          <LoansItem data={data[0]}/>
-          {/* <li>
-            <div>
-              <p>Loan name {data[0].id}</p>
-              <p>Loan detalis</p>
-            </div>
-            <div className="buttons">
-              <span>invest</span>
-              <button className="btn btn-warning">INVEST</button>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p>Loan name 1</p>
-              <p>Loan detalis</p>
-            </div>
-            <div className="buttons">
-              <span>invest</span>
-              <button className="btn btn-warning">INVEST</button>
-            </div>
-          </li>
-          <li>
-            <div>
-              <p>Loan name 1</p>
-              <p>Loan detalis</p>
-            </div>
-            <div className="buttons">
-              <span>invest</span>
-              <button className="btn btn-warning">INVEST</button>
-            </div>
-          </li> */}
-        </ul>
-        <p className="total"> Total amount available for investmant: ${total} </p>
-      </div>
+    <div className="wrapper">
+      <ul>{loanList}</ul>
+      <p className="total"> Total amount available for investmant: ${total} </p>
+    </div>
   );
 };
 
